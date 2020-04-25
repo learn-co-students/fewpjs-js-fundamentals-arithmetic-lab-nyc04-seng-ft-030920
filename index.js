934d71a4459624d76b86c35cb202eb40547e2978
Xavier Carty <xaviercarty@gmail.com>
2020-04-25 17:05:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31; 
+var multiply = num1 * num2; 
+
+var random = Math.random(); 
+var mod  = 16 % 6; 
+
+var max = Math.max(1 ,2  ,4  , 5 ,20)
